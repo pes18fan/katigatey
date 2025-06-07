5fc1a1d086cc58293302a3254296e5315cd1b942
pes18fan <98473098+pes18fan@users.noreply.github.com>
2025-06-07 21:26:19 +0545

    chore: use simpler language and fix version

--- lib/pages/view.dart
+++ lib/pages/view.dart
@@ -25,11 +25,11 @@ class _DateViewState extends State<DateView> {
     return Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text("Today is ", style: textStyle),
+        Text("Today's date is ", style: textStyle),
         Text(today.toString(), style: dateStyle)
       ]),
       Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text("Gregorian: ", style: textStyle),
+        Text("English date is ", style: textStyle),
         Text(formattedGregorian, style: dateStyle)
       ]),
     ]));
